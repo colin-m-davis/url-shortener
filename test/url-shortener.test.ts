a89cc524758fff4d67d586541f7271962cc5b513
colindavis <colinmichaelsdavis@gmail.com>
2023-07-06 08:49:31 -0700

    add unit tests for cdk synthesis

--- test/url-shortener.test.ts
+++ test/url-shortener.test.ts
@@ -1,17 +1,29 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as UrlShortener from '../lib/url-shortener-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as UrlShortener from '../lib/url-shortener-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/url-shortener-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new UrlShortener.UrlShortenerStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('UrlShortenerStack', () => {
+    test('synthesis', () => {
+        const app = new cdk.App();
+        const stack = new UrlShortener.UrlShortenerStack(app, 'MyTestStack');
+        const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+        template.hasResourceProperties("AWS::Lambda::Function", {
+            Handler: 'lambda.generate',
+            Runtime: 'nodejs18.x',
+        });
+
+        template.hasResourceProperties("AWS::Lambda::Function", {
+            Handler: 'lambda.go',
+            Runtime: 'nodejs18.x',
+        });
+    });
+
+    test('matches the snapshot', () => {
+        const app = new cdk.App();
+        const stack = new UrlShortener.UrlShortenerStack(app, 'MyTestStack');
+        const template = Template.fromStack(stack);
+
+        expect(template.toJSON()).toMatchSnapshot();
+    });
 });
